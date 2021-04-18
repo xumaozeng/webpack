580cb054b7ff0c1165774fa676b8be3038523491
xumaozeng <15611895341@163.com>
2021-04-18 22:46:06 +0800

    webpack-03

--- webpack-03/src/index.js
+++ webpack-03/src/index.js
@@ -1,1 +1,11 @@
+import less from "./css/index.less";
+// import pic from "./images/logo.png";
+
 console.log("芳芳别生气");
+/* console.log(pic);
+
+const image = new Image();
+image.src = pic;
+
+const app = document.getElementById("app");
+app.append(image); */
--- webpack-03/webpack.config.js
+++ webpack-03/webpack.config.js
@@ -5,6 +5,7 @@
 const path = require("path");
 const htmlwebpackplugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const minicss = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -13,8 +14,43 @@ module.exports = {
     filename: "[name].js"
   },
   mode: "development",
-  module: {},
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|webp)$/,
+        use: {
+          // loader: "file-loader",
+          loader: "url-loader", // url-loader就是file-loader的变体，内部会自动调用file-loader
+          options: {
+            name: "[name].[ext]",
+            outputPath: "images/", // 图片的输出位置，也就是存放位置
+            publicPath: "../images/", // 图片的引用位置，css中如何引用图片
+            // 图片的路径+图片的名称 ../images/ + logo.png
+            limit: 1024 * 3 // 3KB，小于3kb使用url-loader，否则使用file-loader
+          }
+        }
+      },
+      {
+        test: /\.woff2$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "css/",
+            publicPath: "./"
+          }
+        }
+      },
+      {
+        test: /\.less$/,
+        use: [minicss.loader, "css-loader", "less-loader"]
+      }
+    ]
+  },
   plugins: [
+    new minicss({
+      filename: "css/[name].css"
+    }),
     new htmlwebpackplugin({
       template: "./src/index.html",
       filename: "index.html"
